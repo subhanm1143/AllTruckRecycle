7806c254e583e1dec0a9095456ce6f3648476025
subhanm1143 <subhanm1143@gmail.com>
2024-06-07 15:08:58 -0700

    Fixing Corsss

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const stripe = require('stripe')(process.env.STRIPE);
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
