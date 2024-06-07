3b9d6a6de919969e86d4bca67e28e958dbfe9f2a
subhanm1143 <subhanm1143@gmail.com>
2024-06-07 14:50:33 -0700

    Trying Database

--- Backend/server.js
+++ Backend/server.js
@@ -12,10 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://alltruckrecycling.onrender.com', // Add the correct frontend origin here
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // If you need to include cookies in the requests
+}));
 app.use(express.json());
 app.use(bodyParser.json());
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
